6b5eeffd68f1300f829484348f6c7f98e8ee3c5e
Brian O'Sullivan <brian.osullivan@live.com>
2015-02-17 22:46:31 +0000

    Adding players

--- studio_game.rb
+++ studio_game.rb
@@ -1,7 +1,9 @@
-greeting = "Welcome"
+name1 = 'larry'
+name2 = 'curly'
+name3 = 'moe'
+health1 = 60
 
-3.times do
-  puts greeting.upcase
-end
+puts "#{name1}'s health is #{health1 / 9.0}"
+puts "#{name1}'s health is #{health1 / 9}"
 
-puts Time.now
+puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
