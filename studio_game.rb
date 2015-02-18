7d4a292ae94cf4a3a47fd787a5011191cc8a1978
Brian O'Sullivan <brian.osullivan@live.com>
2015-02-18 21:37:08 +0000

    Adding a fourth player

--- studio_game.rb
+++ studio_game.rb
@@ -1,9 +1,28 @@
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+puts "The game started on #{formatted_time}"
+
 name1 = 'larry'
 name2 = 'curly'
 name3 = 'moe'
+name4 = 'shemp'
+
 health1 = 60
+health2 = 125
+health3 = 100
+health4 = 90
 
-puts "#{name1}'s health is #{health1 / 9.0}"
-puts "#{name1}'s health is #{health1 / 9}"
+puts "#{ name1.capitalize } has a health of #{ health1 }."
+puts "#{ name2.upcase } has a health of #{ health2 }."
 
-puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
+health2 = health1
+puts "#{ name2.upcase } has a health of #{ health2 }."
+
+health1 = 30
+
+puts "#{ name1.capitalize } has a health of #{ health1 }."
+puts "#{ name2.upcase } has a health of #{ health2 }."
+
+puts "#{ name3.capitalize } has a health of #{ health3 }.".center(50, '*')
+
+puts "#{ name4.capitalize.ljust(33, '.') } #{ health4 } health"
