97267f8523edf7a6eafbce68593ab5a704af3779
Brian O'Sullivan <brian.osullivan@live.com>
2015-02-20 00:02:15 +0000

    Adding blocks to iterate over an array of players

--- studio_game.rb
+++ studio_game.rb
@@ -10,10 +10,12 @@ class Player
   
   def blam
     @health -= 10
+    puts "#{@name} got blammed"
   end
   
   def w00t
     @health += 15
+    puts "#{@name} got w00ted"
   end
   
   def name=(new_name)
@@ -23,7 +25,7 @@ class Player
   def score
     @health + @name.length
   end
-  
+
   def to_s
     "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
@@ -31,15 +33,28 @@ class Player
 end
 
 player1 = Player.new("moe")
-puts player1
-
 player2 = Player.new("larry", 60)
-puts player2
+player3 = Player.new("curly", 125)
 
-player2 = Player.new("larry", 60)
-puts player2.name
-player2.name = "lawrence"
-puts player2.name
-puts player2.health
-puts player2.score
-puts player2
+players = [player1, player2, player3]
+
+puts "There are #{players.size} players in our game."
+
+players.each do |player|
+  puts player
+end
+
+players.each do |player|
+  puts player.health
+end
+
+players.each do |player|
+  player.w00t
+  player.blam
+  player.w00t
+  puts player
+end
+
+players.pop
+players.push Player.new "Schemp"
+puts players
