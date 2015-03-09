ab1073fa412764107e16f0bdd18041012b3c2463
Brian O'Sullivan <brian.osullivan@live.com>
2015-03-09 00:25:34 +0000

    Adding a Game class to manage players and play

--- studio_game.rb
+++ studio_game.rb
@@ -1,27 +1,51 @@
+class Game
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player player
+    @players << player
+  end
+
+  def play
+    puts @players
+
+    @players.each do |player|
+      player.w00t
+      player.blam
+      player.blam
+      puts player
+    end
+  end
+
+end
+
+
 class Player
-  
+
   attr_reader :health
   attr_accessor :name
-  
+
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
-  
+
   def blam
     @health -= 10
     puts "#{@name} got blammed"
   end
-  
+
   def w00t
     @health += 15
     puts "#{@name} got w00ted"
   end
-  
+
   def name=(new_name)
     @name = new_name.capitalize
   end
-  
+
   def score
     @health + @name.length
   end
@@ -29,32 +53,16 @@ class Player
   def to_s
     "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
-  
+
 end
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-players = [player1, player2, player3]
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
 
-puts "There are #{players.size} players in our game."
-
-players.each do |player|
-  puts player
-end
-
-players.each do |player|
-  puts player.health
-end
-
-players.each do |player|
-  player.w00t
-  player.blam
-  player.w00t
-  puts player
-end
-
-players.pop
-players.push Player.new "Schemp"
-puts players
