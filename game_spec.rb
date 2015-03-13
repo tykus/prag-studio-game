2116071301b8f7a28f2fc0cedbb56e55607f630e
Brian O'Sullivan <brian.osullivan@live.com>
2015-03-13 00:18:41 +0000

    Adding a set of tests for the Game class

---
+++ game_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'game'
+
+describe Game do
+
+  before do
+    @game = Game.new("Knuckleheads")
+
+    @initial_health = 100
+    @player = Player.new("moe", @initial_health)
+
+    @game.add(@player)
+  end
+
+  
+
+end
