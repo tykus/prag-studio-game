d42142ec08aea2aedc8d4419f8b6cf17d5eac9af
Brian O'Sullivan <brian.osullivan@live.com>
2015-03-27 11:28:15 +0000

    Printing Player stats after each Game has been played

--- game.rb
+++ game.rb
@@ -12,10 +12,27 @@ class Game
     @players << player
   end
 
-  def play
-    @players.each do |player|
-      GameTurn.take_turn(player)
-      puts player
+  def play(rounds)
+    1.upto(rounds) do |round|  
+      puts "\nRound #{round}:"
+      @players.each do |player|
+        GameTurn.take_turn(player)
+        puts player
+      end
+    end
+  end
+  
+  def print_stats
+    strong_players, wimpy_players = @players.partition { |p| p.strong? }
+    
+    puts "\n#{ @title } Statistics:"
+    puts "#{ strong_players.count } strong players:"
+    strong_players.each do |player|
+      puts "#{ player.name } (#{ player.health })"
+    end
+    puts "#{ wimpy_players.count } wimpy players:"
+    wimpy_players.each do |player|
+      puts "#{ player.name } (#{ player.health })"
     end
   end
 
