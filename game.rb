a626a2b16dcf2007398e76aa108dc01679007ffb
Brian O'Sullivan <brian.osullivan@live.com>
2015-04-04 13:03:16 +0100

    Adding treasures

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'game_turn'
+require_relative 'treasure_trove'
 
 class Game
 
@@ -13,7 +14,14 @@ class Game
   end
 
   def play(rounds)
-    1.upto(rounds) do |round|  
+    treasures = TreasureTrove::TREASURES
+    puts "There are #{treasures.size} treasures to be found:"
+    treasures.each do |treasure|
+      puts "A #{treasure.name} is worth #{treasure.points} points"
+    end
+
+
+    1.upto(rounds) do |round|
       puts "\nRound #{round}:"
       @players.each do |player|
         GameTurn.take_turn(player)
@@ -21,10 +29,10 @@ class Game
       end
     end
   end
-  
+
   def print_stats
     strong_players, wimpy_players = @players.partition { |p| p.strong? }
-    
+
     puts "\n#{ @title } Statistics:"
     puts "#{ strong_players.count } strong players:"
     strong_players.each do |player|
@@ -34,15 +42,15 @@ class Game
     wimpy_players.each do |player|
       puts print_name_and_health(player)
     end
-    
+
     # High Scores
     puts "\n#{ @title } High Scores:"
     @players.sort.each do |player|
       formatted_name = player.name.ljust(20, '.')
       puts "#{formatted_name} #{player.score}"
-    end 
+    end
   end
-  
+
   private
     def print_name_and_health(player)
       "#{ player.name } (#{ player.health })"
