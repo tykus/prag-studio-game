a8acbb99329ead273b729f91c48e0494c658b6ea
Brian O'Sullivan <brian.osullivan@live.com>
2015-04-17 10:46:43 +0100

    Looking for user input for number of rounds to be played

--- studio_game.rb
+++ studio_game.rb
@@ -1,14 +1,26 @@
 require_relative 'game'
 
+knuckleheads = Game.new("Knuckleheads")
+
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-knuckleheads = Game.new("Knuckleheads")
 knuckleheads.add_player(player1)
 knuckleheads.add_player(player2)
 knuckleheads.add_player(player3)
-knuckleheads.play(10) do
-  knuckleheads.total_points >= 2000
+
+loop do
+  puts "How many game rounds? ('quit' to exit)"
+  answer = gets.chomp
+
+  case answer
+  when /^\d+$/
+    knuckleheads.play(answer.to_i)
+    knuckleheads.print_stats
+  when 'quit', 'exit'
+    break
+  else
+    "Please enter a number or 'quit'!"
+  end
 end
-knuckleheads.print_stats
